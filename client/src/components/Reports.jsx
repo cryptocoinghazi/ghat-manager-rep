870cd1a42521ad1a351b4653823ef914dd0a9f24
cryptocoinghazi <155804990+cryptocoinghazi@users.noreply.github.com>
2025-12-10 11:12:55 +0000

    chnages

--- client/src/components/Reports.jsx
+++ client/src/components/Reports.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 import {
   FiFilter,
   FiDownload,
